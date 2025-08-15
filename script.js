7811414f2c36c39edcd22c7dceaa7ae5fb037815
Leo <garcia117044@insuco.edu.mx>
2025-08-15 14:27:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Menú hamburguesa
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
